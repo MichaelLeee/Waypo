bdc1bd76d249c9b6c7251d437bf4d06ec7e3a581
MichaelLeee <michaellee0106@gmail.com>
2026-09-07 09:56:34 -0700

    add Widget Extension

---
+++ WaypoWidget/WaypoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WaypoWidgetBundle.swift
+//  WaypoWidget
+//
+//  Created by Michael on 9/7/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WaypoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WaypoWidget()
+    }
+}
